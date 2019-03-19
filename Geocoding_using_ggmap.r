0c424da53ad0aa03fd8c8bc00760f52e57cb4a15
ShruSM <ShruSM@users.noreply.github.com>
2019-03-19 09:42:06 -0500

    Add files via upload
    This is a script to do geocoding using google maps API. 
    Input: Address strings. Example: 2401 S University Dr, Fort Worth, TX 76109
    Output: Latitude and Longitude

---
+++ Geocoding_using_ggmap.r
@@ -0,0 +1,34 @@
+######install the required packages#########
+install.packages("ggmap")
+install.packages("devtools")
+library(ggmap)
+
+##SET WORKING DIRECTORY##
+setwd("C:\\Folder\\Folder\\Documents\\Folder\\Geocoding")
+devtools::install_github("dkahle/ggmap")
+
+####Obatin google key by activating geocoding API and creating new credential key######
+register_google(key = "Google API key")     ## Looks something like this: WqDiSyAwfuiT5mwNmHXUN9Idd1V56MB1C4LZlFAE
+
+###READ DATA: csv file with addresses. sample: 4010 Bryce Ave, Fort Worth, TX 76107 ###
+data <- read.csv("addresses.csv", header=TRUE, stringsAsFactors = FALSE)
+address_list <- unique(data$addresses)
+
+###CREATING A DATA FRAME TO APPEND VALUES TO ###
+address <- vector()
+lat <- vector()
+long <- vector()
+
+###GEOCODE ###
+for (i in 1:length(address_list)) 
+{
+  coord <- geocode(address_list[i])
+  address <- append(address,address_list[i])
+  lat <- append(lat,coord[2])
+  long <- append(long,coord[1])
+}
+
+data_frame=data.frame(cbind(address,lat,long),stringsAsFactors=FALSE)
+
+###Write output to file
+write.table(data_frame, geocoding_results.txt, row.names = F, sep = "\t" )
